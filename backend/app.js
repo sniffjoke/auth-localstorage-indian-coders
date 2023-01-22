01d18d3dfe26401e7a6a1f93a91166e9942ae8df
sniffjoke <41764936+sniffjoke@users.noreply.github.com>
2023-01-22 22:26:33 +0100

    add config env

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,11 @@ import express from 'express'
 import mongoose from "mongoose";
 import router from "./routes/user-routes.js";
 import blogRouter from "./routes/blog-routes.js";
+require('dotenv').config()
 import cors from 'cors'
+
+const MONGO_USER = process.env.MONGO_USER
+const MONGO_PASSWORD = process.env.MONGO_PASSWORD
 
 const app = express()
 
@@ -12,7 +16,7 @@ app.use(express.json())
 app.use('/api/user', router)
 app.use('/api/blog', blogRouter)
 
-mongoose.connect('mongodb+srv://nalcapital:sZRB1KJHUJqTrFg8@cluster0.ggcw4.mongodb.net/indicodersauthlocalst?retryWrites=true&w=majority')
+mongoose.connect(`mongodb+srv://${MONGO_USER}:${MONGO_PASSWORD}@cluster0.ggcw4.mongodb.net/indicodersauthlocalst?retryWrites=true&w=majority`)
     .then(() => app.listen(5000))
     .then(() => console.log('Connected to Database and Listening to Localhost 5000'))
     .catch((err) => console.log(err))
